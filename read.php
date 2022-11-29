2bb682a98de7f39735e0443d615cc46f088163ba
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-11-29 21:48:02 +0900

    implement change order

--- read.php
+++ read.php
@@ -6,7 +6,19 @@ $password = 'root';
 try {
   $pdo = new PDO($dsn, $user, $password);
 
-  $sql_select = 'SELECT * FROM vocaburaries';
+  // orderパラメータの値が存在すれば(並び替えボタンを押したとき)、その値を変数$orderに代入する
+  if (isset($_GET['order'])) {
+    $order = $_GET['order'];
+  } else {
+    $order = NULL;
+  }
+
+  // パラメータの値によってSQL文を変更する
+  if ($order === 'desc') {
+    $sql_select = 'SELECT * FROM vocaburaries ORDER BY add_date DESC';
+  } else {
+    $sql_select = 'SELECT * FROM vocaburaries ORDER BY add_date ASC';
+  }
 
   $stmt_select = $pdo->query($sql_select);
 
@@ -37,12 +49,18 @@ try {
       <h1>単語一覧</h1>
       <div class="vocaburaries-ui">
         <div>
-          <!-- 並び替えボタンと検索ボタンを作成する -->
+          <a href="read.php?order=desc">
+            <img src="images/desc.png" alt="降順に並び替え" class="sort-img">
+          </a>
+          <a href="read.php?order=asc">
+            <img src="images/asc.png" alt="昇順に並び替え" class="sort-img">
+          </a>
         </div>
         <a href="#" class="btn">単語登録</a>
       </div>
       <table class="vocaburaries-table">
         <tr>
+          <th>追加日付</th>
           <th>語彙</th>
           <th>品詞</th>
           <th>意味</th>
@@ -51,6 +69,7 @@ try {
         foreach ($vocaburaries as $vocaburary) {
           $table_row = "
             <tr>
+              <td>{$vocaburary['add_date']}</td>
               <td>{$vocaburary['vocaburary']}</td>
               <td>{$vocaburary['type']}</td>
               <td>{$vocaburary['meaning']}</td>
